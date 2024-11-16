dd47a49028f05bd326366a0d9d890416d2fdb648
Francisco Santander <71709143+furanshis@users.noreply.github.com>
2024-11-16 13:41:09 -0300

    cambios

---
+++ registro-asistencia/src/app/pin-modal/pin-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-modal',
+  templateUrl: './pin-modal.component.html',
+  styleUrls: ['./pin-modal.component.scss'],
+})
+export class PinModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
