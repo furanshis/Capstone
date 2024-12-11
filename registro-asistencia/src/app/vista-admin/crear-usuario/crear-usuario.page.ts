d8e51bef706879d688d7fd1b1cf55e191960a726
Francisco Santander <71709143+furanshis@users.noreply.github.com>
2024-12-11 00:56:18 -0300

    mejoras vista admin

--- registro-asistencia/src/app/vista-admin/crear-usuario/crear-usuario.page.ts
+++ registro-asistencia/src/app/vista-admin/crear-usuario/crear-usuario.page.ts
@@ -10,6 +10,7 @@ import { EmpleadoService } from '../../services/empleado.service';
   styleUrls: ['./crear-usuario.page.scss'],
 })
 export class CrearUsuarioPage  {
+  
 
 
   email: string = '';
@@ -20,86 +21,74 @@ export class CrearUsuarioPage  {
   apellido_materno: string = '';
   horario_entrada_real: string = '';
   horario_salida_real: string = '';
-  supervisor: number = 0;
-  departamento: number = 0;
-  pin: number = 1234;
+  pinpass: number = 1234;
   goBack(){
     window.history.back();
   }
 
 
   constructor(
-    private afAuth: AngularFireAuth,
+    private toastCtrl: ToastController,
+
+    private auth: AngularFireAuth,
     private firestore: AngularFirestore,
     private toastController: ToastController,
     private empleadoService: EmpleadoService
   ) {}
 
-  async addEmployee(): Promise<any> {  
+  async crearEmpleado() {
+    if (!this.email.endsWith('@duocuc.cl')) {
+      this.showToast('El correo debe ser del dominio @duocuc.cl', 'danger');
+      console.error('El correo debe ser del dominio @duocuc.cl');
+      return;
+    }
     try {
-      // Crear usuario en Firebase Authentication
-      const userCredential = await this.afAuth.createUserWithEmailAndPassword(this.email, this.password);
+      const userCredential = await this.auth.createUserWithEmailAndPassword(
+        this.email,
+        this.password
+      );
 
       const user = userCredential.user;
 
-      if (user) {
-        
+      if (user){
         const displayName = `${this.primer_nombre} ${this.apellido_paterno}`;
-        
         await user.updateProfile({ displayName: displayName });
-        
       }
-      const uid = userCredential.user?.uid;
 
+      const uid = userCredential.user?.uid;
       if (uid) {
         
-        const empleado = {
-          uid_empleado: uid,
+
+        
+        await this.firestore.collection('empleado').doc(uid).set({
           primer_nombre: this.primer_nombre,
           segundo_nombre: this.segundo_nombre,
           apellido_paterno: this.apellido_paterno,
           apellido_materno: this.apellido_materno,
           horario_entrada_real: this.horario_entrada_real,
           horario_salida_real: this.horario_salida_real,
-          supervisor: this.supervisor,
-          departamento: this.departamento,
-          pin: this.pin,
-        }
-        console.log(empleado.horario_entrada_real)
-        
-        await this.empleadoService.crearEmpleado(empleado).toPromise();
-        console.log()
-
-        await this.afAuth.signOut();
-        
-
-        // Mostrar notificación de éxito
-        const toast = await this.toastController.create({
-          message: 'Empleado agregado exitosamente',
-          duration: 2000,
-          color: 'success'
+          pinpass: this.pinpass,
+          email: this.email,
         });
-        toast.present();
-        
-        // Limpiar el formulario
-        this.email = '';
-        this.password = '';
-        this.primer_nombre = '';
-        this.segundo_nombre = ''; 
-        this.apellido_paterno = '';
-        this.apellido_materno = '';
-        this.horario_entrada_real = '';
-        this.horario_salida_real = '';
+        this.showToast('Empleado creado correctamente.', 'success');
+        console.log('Empleado creado correctamente.');
       }
     } catch (error) {
-      console.error('Error al agregar empleado:', error);
-      const toast = await this.toastController.create({
-        message: 'Error al agregar empleado',
-        duration: 2000,
-        color: 'danger'
-      });
-      toast.present();
+      this.showToast('error creando usuario', 'danger');
+      console.error('Error creando empleado:', error);
     }
+  }
+
+ 
+
+  private async showToast(message: string, color: string): Promise<void> {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 3000,
+      color,
+      position: 'top'
+    });
+    await toast.present();
   }
 
 }
--- registro-asistencia/src/app/vista-admin/registro-asistencia/registro-asistencia.page.ts
+++ registro-asistencia/src/app/vista-admin/registro-asistencia/registro-asistencia.page.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Reporte } from '../../interfaces/models';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
 
 @Component({
   selector: 'app-registro-asistencia',
@@ -10,7 +13,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./registro-asistencia.page.scss'],
 })
 export class RegistroAsistenciaPage implements OnInit {
-  reportes: Reporte[] = [];
+  reportes: any[] = [];
   reportesFiltrados: any[] = []; // Reportes después de aplicar filtros
 
   filtro = {
@@ -19,13 +22,49 @@ export class RegistroAsistenciaPage implements OnInit {
   };
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private firestore: AngularFirestore) {}
 
   ngOnInit() {
+    this.cargarReportes();
     this.http.get<Reporte[]>('http://192.168.1.85:3000/reporte-asistencia').subscribe(
       data => this.reportes = data
     );
     
+  }
+
+  cargarReportes() {
+    this.obtenerReportesConEmpleados().subscribe({
+      next: (data) => {
+        this.reportes = data;
+        console.log('Datos cargados:', this.reportes);
+      },
+      error: (error) => console.error('Error al cargar reportes:', error),
+    });
+  }
+
+  obtenerReportesConEmpleados(): Observable<any[]> {
+    return this.firestore
+      .collection('reporte_asistencia')
+      .valueChanges({ idField: 'id' })
+      .pipe(
+        switchMap((reportes: any[]) => {
+          const consultas = reportes.map((reporte) =>
+            from(
+              this.firestore
+                .collection('empleado')
+                .doc(reporte.uid)
+                .get()
+                .toPromise()
+            ).pipe(
+              map((empleadoDoc) => ({
+                ...reporte,
+                empleado: empleadoDoc?.exists ? empleadoDoc.data() : null,
+              }))
+            )
+          );
+          return from(Promise.all(consultas));
+        })
+      );
   }
 
   aplicarFiltros() {
